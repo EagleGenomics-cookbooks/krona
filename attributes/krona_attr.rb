84f9373084ff446c7f2a4fa5ff1cc5681fb4f261
Nick James <nick.james@eaglegenomics.com>
2017-05-05 12:17:15 +0100

    working installation

--- attributes/krona_attr.rb
+++ attributes/krona_attr.rb
@@ -1,3 +1,3 @@
-default['krona']['src_dir'] = '/usr/local/krona'
+default['krona']['src_dir'] = '/usr/local/krona_src'
 default['krona']['src_repo'] = 'https://github.com/marbl/Krona.git'
 default['krona']['version'] = 'v2.7'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,6 +4,8 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
+include_recipe 'apt'
+include_recipe 'build-essential'
 include_recipe 'git'
 
 git 'checkout krona src code' do
@@ -11,4 +13,19 @@ git 'checkout krona src code' do
   repository node['krona']['src_repo']
   revision node['krona']['version']
   action 'export'
+end
+
+execute 'install krona' do
+  command 'perl install.pl'
+  cwd "#{node['krona']['src_dir']}/KronaTools"
+end
+
+# Create the taxonomy dir needed by updateTaxonomy
+directory "#{node['krona']['src_dir']}/KronaTools/taxonomy" do
+  action :create
+end
+
+execute 'run updateTaxonomy.sh to update taxonomy' do
+  command './updateTaxonomy.sh'
+  cwd "#{node['krona']['src_dir']}/KronaTools"
 end
